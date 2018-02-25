bde193f09722fa6945ca41f84ca14e2f650bfd1f
Stanislau-Tuchto <toshiro_mifune@mail.ru>
2018-02-25 16:13:55 +0300

    implement fuction check 8ms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    arrB = [];
+    for(let i=0, l = str.length; i < l; i++){
+        for (let j=0, lb = bracketsConfig.length; j < lb; j++){
+           if((str[i] === bracketsConfig[j][0] && str[i] !== bracketsConfig[j][1])|
+               (str[i] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1] && str[i]!==arrB[arrB.length-1])){
+               arrB.push(str[i]);
+               break;
+           }else if(str[i] === bracketsConfig[j][1]){
+                if(arrB[arrB.length-1] === bracketsConfig[j][0]){
+                    arrB.splice(arrB.length-1,1);
+                    break;
+                } else {
+                    return false;
+                }
+           }
+        }
+    }
+    return !arrB.length;
+
 }
